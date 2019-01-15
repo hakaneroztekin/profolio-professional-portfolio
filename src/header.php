e818ae4aa68536d65c142b5b2a6d0560e099e992
hakaneroztekin <eroztekin@itu.edu.tr>
2019-01-15 19:33:29 +0300

    refactor to PHP (for Wordpress)

--- src/header.php
+++ src/header.php
@@ -3,5 +3,12 @@
     <head>
         <title>Welcome | Hakan Eroztekin</title>
         <link href="/wp-content/themes/portfolio/style.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Cormorant:500" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css?family=Cormorant:500" rel="stylesheet">
+        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     </head>
--- src/index.php
+++ src/index.php
@@ -25,21 +25,180 @@
                 <br>
                 <a href="/wp-content/uploads/2019/01/HakanEroztekinCV.pdf">Download<br>My CV</a>
                 <br><br>
-                <p>This page is under development since 14.01.2019</p>
             </div>
         </div>
 
     </div>
 
-    <div id="main-content">
+    <div id="sections">
+
+
+        <section id="about"> 
+            <div class="section-title">ABOUT</div>
+            <img src="/wp-content/themes/portfolio/images/vertical-split.png" alt="vertical split">
+            <p>Hello! I'm <b>Hakan</b>, an enthusiastic final year computer engineering student at Istanbul Technical University. I am a java developer. I will be available for work (4 or 5 days a week) starting from February 2019.</p>  
+        </section>
+
+        <section id="skills">
+            <div class="section-title">MY SKILLS</div>
+            <img src="/wp-content/themes/portfolio/images/vertical-split.png" alt="vertical split">
+
+            <div class="w3-responsive">
+                <ul class="skills-table">
+                    <li>JAVA
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:90%"></div></div></li>
+                    <li>SPRING
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:85%"></div>
+                        </div></li>
+                    <li>SQL
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:85%"></div>
+                        </div></li>
+                </ul>
+                <ul class="skills-table">
+                    <li>PYTHON
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:90%"></div></div></li>
+                    <li>LINUX
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:80%"></div>
+                        </div></li>
+                    <li>HTML/CSS
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:80%"></div>
+                        </div></li>
+                </ul>
+                <ul class="skills-table">
+                    <li>C/C++
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:95%"></div></div></li>
+                    <li>JIRA / CONFLUENCE
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:85%"></div>
+                        </div></li>
+                    <li>GITHUB / BITBUCKET
+                        <div class="progress">
+                            <div class="progress-bar bg-dark" style="width:90%"></div>
+                        </div></li>
+                </ul>
+            </div>    
+        </section>
+
+        <section id="education-experience">
+
+            <div id="education-experience-background">
+                <img class ="bg" src="/wp-content/themes/portfolio/images/service-bg.png" alt="banner">
+                <div class="banner-overlay" style="padding: 20%">
+                    <div class="section-title">EDUCATION & EXPERIENCE</div>
+                    <img src="/wp-content/themes/portfolio/images/vertical-split.png" alt="vertical split">
+                </div>
+            </div>
+
+            <ul>
+                <li class="li-brief">
+                    <img src="/wp-content/themes/portfolio/images/education-bg.jpg">
+                    <br>
+                    <p>EDUCATION</p>        
+                </li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Computer Engineering (B.Sc.) </p>
+                        <p class="alt-title">Istanbul Technical University, 2013 - 2019 June (expected) </p>
+                    </span>
+                    <span class="show-details-button" tabindex="0">Show Details</span>
+
+                    <span class="details">
+                        <span class="hide-details-button" tabindex="0">Hide Details</span>
+                        <p class="details-content"><br><br>After I ranked <b>top 0.1% among 2 million participants</b> in the National University Entrance Exam in Turkey, I started my education at ITU.</p></span>
+                </li>
+            </ul>
+
+
+
+
+            <ul>
+                <li class="li-brief">
+                    <img src="/wp-content/themes/portfolio/images/experience-bg.png">
+                    <br>
+                    <p>EXPERIENCE</p>        
+                </li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Java Development Intern</p>
+                        <p class="alt-title">adesso Turkey, July 2018 – Aug 2018</p>
+                    </span>
+                    <span class="show-details-button" tabindex="0">Show Details</span>
+
+                    <span class="details">
+                        <span class="hide-details-button" tabindex="0">Hide Details</span>
+                        <p class="details-content"><br><br>As a member of the global team, developed <b>back-end side</b> of an internal project for the company.	
+                            <br><b>Technology stack:</b> Java 8, Spring Boot, Spring Security, Spring Batch, RESTful API, Hibernate, Maven, Lombok, Quartz, SQL, JSON, IntelliJ IDEA, Heidi SQL, Swagger UI, Postman, Sourcetree, Github
+                        </p></span>
+                </li>
+            </ul>
+            <ul>
+                <li class="li-brief"></li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Android Development Intern</p>
+                        <p class="alt-title">Istanbul Technical University, June 2018 – July 2018 </p>
+                    </span>
+                    <span class="show-details-button" tabindex="0">Show Details</span>
+
+                    <span class="details">
+                        <span class="hide-details-button" tabindex="0">Hide Details</span>
+                        <p class="details-content"><br><br>Worked as an Android Developer in an <b>innovative wearable technology</b> project under the supervision of Asst. Prof. Gökhan İnce. <b>Developed an Android application</b> that provides <b>real time visual information</b> to the user.  My graduation project on <b>artificial intelligence</b> is continuum of this project.
+                            <br><b>Technology stack:</b> Java, Android Studio, C/C++, Arduino</p></span>
+                </li>
+            </ul>
+            <ul>
+                <li class="li-brief"></li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Research Development Intern
+
+                        </p>
+                        <p class="alt-title">Medianova CDN, Jan 2018 - May 2018
+
+                        </p>
+                    </span>
+                    <span class="show-details-button" tabindex="0">Show Details</span>
+
+                    <span class="details">
+                        <span class="hide-details-button" tabindex="0">Hide Details</span>
+                        <p class="details-content"><br><br>During my internship at Medianova, I worked on a project where we have developed more efficient routing pathways using <b>modified shortest path algorithms</b>. I also did research about <b>GPU computing, image and video compression </b> and <b>artificial intelligence</b>. Further, I did <b>regular documentation</b> and a <b>presentation</b> about my work.</span>
+                </li>
+            </ul>
+            <ul>
+                <li class="li-brief"></li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Research Development Team Lead</p>
+                        <p class="alt-title">ITU Entrepreneurship Club, Aug 2016 – Jan 2017 </p>
+                    </span>
+                </li>
+            </ul>
+            <ul>
+                <li class="li-brief"></li>
+                <li class="li-details">
+                    <span class="titles">
+                        <p class="title">Research Development Team Member
+
+                        </p>
+                        <p class="alt-title">ITU Entrepreneurship Club, Jan 2016 – Jan 2017</p>
+                    </span>
+                </li>
+            </ul>
+
+        </section>
+
+        <section><p></p></section>
+
+        <br><br>
+
     </div>
-
-    <section id="about-me"> 
-    </section>
-    
-    <section></section>
-    <section></section>
-
 
 </body>
 
