5ac71d98a626d0bc6f2280a483434572cc1e1750
hakaneroztekin <eroztekin@itu.edu.tr>
2019-02-27 15:55:55 +0300

    fix margins

--- src/index.php
+++ src/index.php
@@ -300,13 +300,13 @@
 
                                     <div class="w3-hide-small">
                                         <img src="wp-content/themes/portfolio/images/project-1.jpg" class="w3-image w3-border w3-grayscale-min w3-hover-grayscale" style="width:90%;" alt="Course Register System">
-                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Course Register System »</div>    
                                         </div></div>
 
                                     <div class="w3-hide-medium w3-hide-large">
                                         <img src="wp-content/themes/portfolio/images/project-1.jpg" class="w3-image w3-border w3-grayscale-max" style="width:90%;" alt="Course Register System">
-                                        <div class="w3-display-position w3-padding"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Course Register System »</div>    
                                         </div>
                                     </div>
@@ -326,13 +326,13 @@
 
                                     <div class="w3-hide-small">
                                         <img src="wp-content/themes/portfolio/images/homepage.png" class="w3-image w3-border w3-grayscale-min w3-hover-grayscale" style="width:90%;" alt="Intellect Database">
-                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Intellect Database »</div>    
                                         </div></div>
 
                                     <div class="w3-hide-medium w3-hide-large">
                                         <img src="wp-content/themes/portfolio/images/homepage.png" class="w3-image w3-border w3-grayscale-max" style="width:90%;" alt="Intellect Database Project">
-                                        <div class="w3-display-position w3-padding"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Intellect Database »</div>    
                                         </div>
                                     </div>
@@ -350,13 +350,13 @@
 
                                     <div class="w3-hide-small">
                                         <img src="wp-content/themes/portfolio/images/project-2.jpg" class="w3-image w3-border w3-grayscale-min w3-hover-grayscale" style="width:90%;" alt="Best Companies in Istanbul">
-                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding w3-display-hover"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Best Companies in Istanbul »</div>    
                                         </div></div>
 
                                     <div class="w3-hide-medium w3-hide-large">
                                         <img src="wp-content/themes/portfolio/images/project-2.jpg" class="w3-image w3-border w3-grayscale-max" style="width:90%;" alt="Best Companies In Istanbul">
-                                        <div class="w3-display-position w3-padding"  style="top:55%;left:17%;">
+                                        <div class="w3-display-position w3-padding"  style="top:55%;left:10%;">
                                             <div class="w3-text-white">Best Companies In Istanbul »</div>    
                                         </div>
                                     </div>
